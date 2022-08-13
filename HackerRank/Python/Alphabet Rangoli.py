e775b29fd4f6d1fb99f5d694815ccce6d7f37e2f
egi_190137 <61105702+egi-190137@users.noreply.github.com>
2022-08-13 21:02:04 +0700

    commit

--- HackerRank/Python/Alphabet Rangoli.py
+++ HackerRank/Python/Alphabet Rangoli.py
@@ -1,12 +1,28 @@
+# Size 5
+# e-d-c-b-a-b-c-d-e
+
+# 3 => 9
+# 5 => 17
+# 10 => 37
+# Rumus length = (size - 1) * 2 - 1
+
 def rangoli(length, max):
     alpha = 'abcdefghijklmnopqrstuvwxyz'
 
-    temp = alpha[:max][:-length+1:-1] + alpha[:max][max-length+1:]
+    temp = alpha[:max][:-length-1:-1] + alpha[:max][max-length+1:]
     return '-'.join(list(temp))
 
 def print_rangoli(size):
-    for i in range(1, size):
-        print(rangoli(i, size))
+    len_rows = ((size-1) * 4) + 1
+    for i in range(1, size+1):
+        temp = rangoli(i, size)
+        padding_width = (len_rows - len(temp)) // 2
+        print("-"*padding_width + temp + "-"*padding_width)
+
+    for i in range(size-1, 0, -1):
+        temp = rangoli(i, size)
+        padding_width = (len_rows - len(temp)) // 2
+        print("-"*padding_width + temp + "-"*padding_width)
 
 if __name__ == '__main__':
     n = int(input())
---
+++ HackerRank/Python/Capitalize.py
@@ -0,0 +1,20 @@
+def solve(s):
+    arr = []
+    temp = ""
+    for ch in s:
+        if temp != "":
+            if ch != " " and temp[-1] == " " or ch == " " and temp[-1] != " ":
+                arr.append(temp)
+                temp = ch
+            else:
+                temp += ch
+        else:
+            temp += ch
+    arr.append(temp)
+    # return arr
+    return "".join(map(lambda x: x.capitalize(), arr))
+
+if __name__ == '__main__':
+    s = input()
+
+    print(solve(s))
