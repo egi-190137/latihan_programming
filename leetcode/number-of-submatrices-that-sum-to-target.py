120c05b8b4423c0bc0008566977e0a89c3987955
egi-190137 <egiputra450@gmail>
2024-01-28 16:13:35 +0700

    commit

--- leetcode/number-of-submatrices-that-sum-to-target.py
+++ leetcode/number-of-submatrices-that-sum-to-target.py
@@ -8,14 +8,14 @@ class Solution:
                 top = 0 if r == 0 else sub_sum[r-1][c] 
                 left = 0 if c == 0 else sub_sum[r][c-1]
                 top_left = 0 if min(r, c) == 0 else sub_sum[r-1][c-1]
-                sub_sum[r][c] = matrix[r][c] + top + left - top_lest
+                sub_sum[r][c] = matrix[r][c] + top + left - top_left
 
 
         res = 0
         for r1 in range(ROWS):
             for r2 in range(r1, ROWS):
                 count = defaultdict(int)
-                count[0] = r-1
+                count[0] = 1
                 for c in range(COLS):
                     cur_sum = sub_sum[r2][c] - (
                         sub_sum[r1-1][c] if r1 > 0 else 0
